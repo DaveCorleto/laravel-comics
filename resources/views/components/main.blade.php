38b1e83a783d639dc434c6b970e3274be69d5bc6
DaveCorleto <davide.corleto@fastwebnet.it>
2024-02-12 16:52:58 +0100

    sass bug fixed

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,13 +1,13 @@
 
 <main>
     <h1>Hello from Main</h1>
-
     <div class="gallery">
     @foreach ($comics as $comic)
+    
         <div class="card-immagine">
             <img src="{{$comic['thumb']}}" alt="">
             <div>{{$comic['title']}}</div> 
         </div>
-    </div>
     @endforeach
+    </div>
 </main>
